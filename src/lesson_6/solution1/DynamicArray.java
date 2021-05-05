b4232049ffc31bfd719cfff7dc9be38883768de6
Shellx686 <61562385+Shellx686@users.noreply.github.com>
2021-05-05 21:47:20 +0300

    Homework to lesson 4

---
+++ src/lesson_6/solution1/DynamicArray.java
@@ -0,0 +1,97 @@
+package lesson_6.solution1;
+
+import lesson_3.solution2.ArrayUtil;
+
+// removeByValue(int value) - удаляет все элементы из массива, которые равны заданному значению.
+// К примеру, removeByValue(3), массив элементов [3, 5, 6, 3 8, 4], в результате должны остаться [5, 6, 8, 4].
+// removeByIndex(int index) - удаляет элемет по идексу
+// trim() - уменьшает размер массива до количества элементов, т.е. Если размер массива 9,
+// а элементов 5, то размер массива должен стать равным 5
+// get(int index) - получение элемента по индексу
+// getSize() - получение размера списка
+// isEmpty() - возвращает true, если список пуст, иначе false
+public class DynamicArray {
+
+    private int[] arr;
+
+    public DynamicArray(int[] arr) {
+        this.arr = arr;
+    }
+
+    public int[] getArr() {
+        return arr;
+    }
+
+    public void setArr(int[] arr) {
+        this.arr = arr;
+    }
+
+    void removeByValue(int value) {
+        int COUNT = 0;
+        int COUNT2 = 0;
+        for (int i = 0; i < getArr().length; i++) {
+            if (value == getArr()[i]) {
+                COUNT++;
+            }
+        }
+        int[] y = new int[getArr().length - COUNT];
+
+        for (int i = 0; i < getArr().length; i++) {
+            if (getArr()[i] != value) {
+                y[COUNT2] = getArr()[i];
+                COUNT2++;
+            }
+        }
+        setArr(y);
+    }
+
+    void removeByIndex(int index) {
+        if (index < getArr().length) {
+            int count = 0;
+            int[] y = new int[getArr().length - 1];
+            for (int i = 0; i < getArr().length; i++) {
+                if (i != index) {
+                    y[count] = getArr()[i];
+                    count++;
+                }
+            }
+            setArr(y);
+        }
+        else System.out.println("В методе removeByIndex индекс элемента массива задан неверно.");
+    }
+
+    void trim() {
+        int count = 0;
+        for (int i = getArr().length - 1; i >= 0; i--) {
+            if (getArr()[i] == 0) {
+                count++;
+            }
+            else break;
+        }
+        int[] y = new int[getArr().length - count];
+        for (int i = 0; i < y.length; i++) {
+            y[i] = getArr()[i];
+        }
+        setArr(y);
+    }
+
+    int get(int index) {
+        if (index < getArr().length) {
+            return getArr()[index];
+        }
+        else return 0;
+    }
+
+    int getSize() {
+        return getArr().length;
+    }
+
+    boolean isEmpty() {
+        int x = 0;
+        for (int i = 0; i < getArr().length; i++) {
+            x += getArr()[i];
+        }
+        if (x == 0) return true;
+        else return false;
+    }
+}
---
+++ src/lesson_6/solution1/Main.java
@@ -0,0 +1,47 @@
+package lesson_6.solution1;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr1 = new int[]{3, 5, 6, 3, 8, 4};
+        int[] arr2 = new int[]{3, 5, 6, 3, 8, 4};
+        int[] arr3 = new int[]{3, 5, 6, 3, 8, 0, 0, 0, 0}; // размер массива 9, элементов 5
+        int[] arr4 = new int[]{3, 2, 22, 11, 23, 2323};
+        int[] arr5 = new int[]{3, 2, 22, 11, 23, 2323};
+        int[] arr6 = new int[]{3, 2, 22, 11, 23, 2323};
+
+        DynamicArray x1 = new DynamicArray(arr1);
+        x1.removeByValue(3);
+
+        for (int i = 0; i < x1.getArr().length; i++) {
+            System.out.print(x1.getArr()[i] + " ");
+        }
+        System.out.println();
+
+        DynamicArray x2 = new DynamicArray(arr2);
+        x2.removeByIndex(3);
+        for (int i = 0; i < x2.getArr().length; i++) {
+            System.out.print(x2.getArr()[i] + " ");
+        }
+        System.out.println();
+
+        DynamicArray x3 = new DynamicArray(arr3);
+        x3.trim();
+        for (int i = 0; i < x3.getArr().length; i++) {
+            System.out.print(x3.getArr()[i] + " ");
+        }
+        System.out.println();
+
+        DynamicArray x4 = new DynamicArray(arr4);
+        int index = x4.get(4);
+        System.out.println(index);
+
+        DynamicArray x5 = new DynamicArray(arr5);
+        int lengthOfArray = x5.getSize();
+        System.out.println(lengthOfArray);
+
+        DynamicArray x6 = new DynamicArray(arr6);
+        boolean completion = x6.isEmpty();
+        System.out.println(completion);
+
+    }
+}
